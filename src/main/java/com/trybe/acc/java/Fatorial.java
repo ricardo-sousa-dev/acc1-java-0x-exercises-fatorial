c448d08f097845efa291da0beee58c2aeb9f28de
ricardo-sousa <rwmsousa@gmail.com>
2022-03-18 14:30:19 -0300

    resolve checkstyles errors

--- src/main/java/com/trybe/acc/java/Fatorial.java
+++ src/main/java/com/trybe/acc/java/Fatorial.java
@@ -5,14 +5,14 @@ public class Fatorial {
    * Método inicial.
    *
    */
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-		long fatorial18 = 1;
+    long fatorial18 = 1;
 
-		for (int i = 1; i <= 18; i++) {
-			fatorial18 *= i;
-		}
+    for (int i = 1; i <= 18; i++) {
+      fatorial18 *= i;
+    }
 
-		System.out.println("O fatorial de 18 é igual a " + fatorial18);
-	}
+    System.out.println("O fatorial de 18 é igual a " + fatorial18);
+  }
 }
